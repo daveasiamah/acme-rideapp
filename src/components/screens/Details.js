01998a551c22f586c2f0041bed71e0d5b82ac4af
daveasiamah7 <daveasiamah7@gmail.com>
2021-09-26 22:37:19 +0000

    adding navigation

---
+++ src/components/screens/Details.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+function Details(){
+    return(
+        <View><Text>Details Screen</Text></View>
+    )
+}
+
+export default Details;
